743ba54370cc8bb9eb6a210c727f7a69296c7a37
marko.gudic <marko.gudic@agilathon.com>
2022-06-01 16:31:59 +0200

    http module

--- src/types/standardDefinition.ts
+++ src/types/standardDefinition.ts
@@ -2,6 +2,7 @@ const _loggerData = require('../settings/loggerData.json')
 const _cacheData = require('../settings/cacheData.json')
 const _notifData = require('../settings/notifData.json')
 const _apiData = require('../settings/apiData.json')
+const _httpData = require('../settings/httpData.json')
 
 interface StandardConfigDefinition {
     item: string,
@@ -14,5 +15,6 @@ const loggerData: StandardConfigDefinition[] = _loggerData
 const cacheData: StandardConfigDefinition[] = _cacheData
 const notifData: StandardConfigDefinition[] = _notifData
 const apiData: StandardConfigDefinition[] = _apiData
+const httpData: StandardConfigDefinition[] = _httpData
 
-export { StandardConfigDefinition, loggerData, cacheData, notifData, apiData }
+export { StandardConfigDefinition, loggerData, cacheData, notifData, apiData, httpData }
