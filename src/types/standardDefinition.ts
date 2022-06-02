a67dddbcbcd969a5f6d684794cd39c3d213cd4cd
Marko Radovcic <mradovcic_ag@medable.com>
2022-06-02 11:15:20 +0200

    documenation link changes

--- src/types/standardDefinition.ts
+++ src/types/standardDefinition.ts
@@ -10,19 +10,7 @@ interface StandardConfigDefinition {
     item: string,
     text: string,
     detail: string,
-    documentation: string,
-    documentationLink?: string
-}
-
-const getDocumentationString = (item: StandardConfigDefinition): string | vscode.MarkdownString => {
-  if (item.documentationLink) {
-    return new vscode.MarkdownString(`
-${item.documentation}   
-### [Documentation](${item.documentationLink})
-        `)
-  }
-
-  return item.documentation
+    documentation: string
 }
 
 const loggerData: StandardConfigDefinition[] = _loggerData
@@ -31,4 +19,4 @@ const notifData: StandardConfigDefinition[] = _notifData
 const apiData: StandardConfigDefinition[] = _apiData
 const httpData: StandardConfigDefinition[] = _httpData
 
-export { StandardConfigDefinition, getDocumentationString, loggerData, cacheData, notifData, apiData, httpData }
+export { StandardConfigDefinition, loggerData, cacheData, notifData, apiData, httpData }
